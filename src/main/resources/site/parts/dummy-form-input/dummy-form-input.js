621b4ba58394557c0d2ea9816cb2155b28bf4181
Rune Forberg <rfo@enonic.com>
2015-09-18 12:39:08 +0200

    save commit for oda

---
+++ src/main/resources/site/parts/dummy-form-input/dummy-form-input.js
@@ -0,0 +1,20 @@
+var portal = require('/lib/xp/portal'); // Import the portal functions
+var thymeleaf = require('/lib/xp/thymeleaf'); // Import the Thymeleaf rendering function
+
+// Handle the GET request
+exports.get = function(req) {
+
+
+    // Specify the view file to use
+    var view = resolve('dummy-form-input.html');
+
+    var model = {};
+
+    // Render HTML from the view file
+    var body = thymeleaf.render(view, model);
+
+    // Return the response object
+    return {
+        body: body
+    }
+};
---
+++ src/main/resources/site/parts/textline/textline.js
@@ -0,0 +1,30 @@
+var portal = require('/lib/xp/portal'); // Import the portal functions
+var thymeleaf = require('/lib/xp/thymeleaf'); // Import the Thymeleaf rendering function
+var UTIL = require('/lib/enonic/util/util');
+// Handle the GET request
+exports.get = function(req) {
+
+
+    // Specify the view file to use
+    var view = resolve('textline.html');
+
+    var component = portal.getComponent();
+    UTIL.log(component);
+
+    var config = component.config;
+
+    var model = {
+        legend: config.legend,
+        label: config.label,
+        name: config.name,
+        type: config.type
+    };
+
+    // Render HTML from the view file
+    var body = thymeleaf.render(view, model);
+
+    // Return the response object
+    return {
+        body: body
+    }
+};
