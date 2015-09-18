d2c177dffc82585e1f93173a8726fef9451ec57e
Rune Forberg <rfo@enonic.com>
2015-09-18 10:51:23 +0200

    Add formbuilder page

--- src/main/resources/site/pages/formbuilder/formbuilder.js
+++ src/main/resources/site/pages/formbuilder/formbuilder.js
@@ -1,15 +1,25 @@
-var thymeleaf = require('/lib/xp/thymeleaf'); // Import the thymeleaf render function
+var portal = require('/lib/xp/portal'); // Import the portal functions
+var thymeleaf = require('/lib/xp/thymeleaf'); // Import the Thymeleaf rendering function
 
 // Handle the GET request
 exports.get = function(req) {
 
-// Specify the view file to use
+    // Get the content that is using the page
+    var content = portal.getContent();
+
+    // Extract the main region which contains component parts
+    var fbRegion = content.page.regions.formbuilder;
+
+    // Specify the view file to use
     var view = resolve('formbuilder.html');
 
-// Render HTML from the view file
-    var body = thymeleaf.render(view, {});
+    var model = {};
+    model.fbRegion = fbRegion;
 
-// Return the response object
+    // Render HTML from the view file
+    var body = thymeleaf.render(view, model);
+
+    // Return the response object
     return {
         body: body
     }
